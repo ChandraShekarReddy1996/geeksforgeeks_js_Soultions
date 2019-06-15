b47bce361ddc17c8df912bed912cc3dd06efe519
Chandra Shekar Reddy <d.chandrasheker1996@gmail.com>
2019-06-15 12:02:44 +0530

    rotate_delete_Array

--- Arrays/delete_element_location.js
+++ Arrays/delete_element_location.js
@@ -1,15 +1,37 @@
-var array = [1,4,3,5,2,6];
+var array = [1,2,3,4,5,6]
+
+/*QUES : To Rotate the Last Element to the First postion while deleting the Second last element in an Array*/
 
 
-var deleteElement = (array,location,cb) => {
-	console.log(array)
+var rotateElement = (array) => {
 	var temp = []
 	array.forEach((item,key) => {
-		if(key != location)
+		if(key == (array.length-1))
 			temp.push(item)
 	})
 
-	console.log(temp)
+	array.forEach((item,key) => {
+		if(key != (array.length-1))
+			temp.push(item)
+	})
+	return temp
 }
 
-deleteElement(array,array.length - 1)
+
+var rotateanddeleteElement = (array) => {
+	console.log("to rotate the element")
+
+	console.log(Math.max(...array))
+
+	while(array.length > 1){
+		console.log(array)
+		array = rotateElement(array)
+		array.pop(array.length-1)
+		
+	}
+
+	console.log('Final --> ',array)
+}
+
+
+rotateanddeleteElement(array)
